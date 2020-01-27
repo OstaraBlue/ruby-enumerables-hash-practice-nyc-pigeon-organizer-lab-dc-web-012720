c4ee65ec01c72fee0106136cfcd054a2f4360435
Sonata Arrington <sarrington1009@gmail.com>
2020-01-27 03:04:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,8 @@
 def nyc_pigeon_organizer(data)
 result = {}
 a = data.map { |key, value|
-  value.map { |key, value|
-   value
+  value.map { |attribute, stats|
+   
   }
     if key
       result = value
