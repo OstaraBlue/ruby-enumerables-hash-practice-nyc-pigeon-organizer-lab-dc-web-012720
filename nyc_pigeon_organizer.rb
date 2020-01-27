644d72020f2bbea5673aae37baf9f8ff3f281518
Sonata Arrington <sarrington1009@gmail.com>
2020-01-27 04:15:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,21 +1,11 @@
 def nyc_pigeon_organizer(data)
-  new_hash = {}
-  data.each do |key, value|
-    value.each do |new_value, names|
-      names.each do |name|
-        
-        if !new_hash[name]
-          new_hash[name] = {}
-        end
-
-        if !new_hash[name][key]
-          new_hash[name][key] = []
-        end
-
-        new_hash[name][key] << new_value.to_s
-
-      end
-    end
+result = {}
+a = data.map { |key, value| 
+  if key == :gender
+    value.map { |g, names| 
+      names
+    }
   end
-  new_hash
+}
+result
 end
